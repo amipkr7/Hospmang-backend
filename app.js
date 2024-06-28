300669dfa5ce801213fc24e0857500888b3fd071
amipkr7 <100938731+amipkr7@users.noreply.github.com>
2024-06-28 23:43:32 +0530

    done some changes

--- app.js
+++ app.js
@@ -12,11 +12,13 @@ import appointmentRouter from './router/appointmentRouter.js';
 const app=express();
 config({path:"./config/config.env"})
 
-app.use(cors({
-    origin:[process.env.FRONTEND_URL,process.env.DASHBOARD_URL],
-    methods:['GET','POST','DELETE','PUT'],
-    credentials:true
-}))
+// app.use(cors({
+//     origin:[process.env.FRONTEND_URL,process.env.DASHBOARD_URL],
+//     methods:['GET','POST','DELETE','PUT'],
+//     credentials:true
+// }))
+
+app.use(cors());
 
 app.use(cookieParser()); 
 app.use(express.json());
